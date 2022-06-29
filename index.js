11d9b1fdef2d36050d7dc3c1d03ac2b347de5ecd
xauvk <xauvkubalexanderyang@live.com>
2022-06-29 12:39:38 -0400

    commit tree e2c7f8395d60145273678e9a1744425262ea920e

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue-42)
+}
+
+function distanceFromHqInFeet(someValue) {
+  return (distanceFromHqInBlocks(someValue) * 264)
+    
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let distance = start - destination
+    distance = Math.abs(distance) * 264
+    return distance
+}
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    let distance = start - destination
+    if(distance<400) {
+        return 'no fee'
+    } else if (distance<2000) {
+        return 2*cent
+    } else if (distance<2500) {
+        return '$25'
+    } else return 'cannot travel that far'
+}
